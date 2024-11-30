fb4d54512f85362e6b0f6c9dfec226efd85d39d8
Vitor <werichvitor@gmail.com>
2024-11-30 02:05:16 -0300

    Adiciona endpoint para export csv de funcionarios

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FuncionarioListAPIView
+from .views import FuncionarioListAPIView, ExportFuncionarioCSV
 
 urlpatterns = [
     path('funcionarios/', FuncionarioListAPIView.as_view(), name='funcionario-list'),
+    path('funcionarios/export/', ExportFuncionarioCSV.as_view(), name='funcionario-export-csv')
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,8 @@
+import csv
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
+from django.http import HttpResponse
 from ..models import Funcionario
 from .serializers import FuncionarioSerializer
 
@@ -13,3 +16,31 @@ class FuncionarioListAPIView(generics.ListAPIView):
     serializer_class = FuncionarioSerializer
 
     pagination_class = FuncionarioPagination
+
+class ExportFuncionarioCSV(APIView):
+    def get(self, request, *args, **kwargs):
+        # Cria uma resposta com tipo de conteúdo CSV
+        resonse_file_name = request.GET.get('file_name', 'funcionarios.csv')
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename="{resonse_file_name}"'
+        
+        # Cria o escritor CSV
+        writer = csv.writer(response)
+        
+        # Cabeçalho do CSV
+        writer.writerow(['ID', 'Nome', 'Data de Nascimento', 'Email', 'Profissão'])
+        
+        # Dados dos funcionários
+        funcionarios = Funcionario.objects.all()
+        
+        for funcionario in funcionarios:
+            writer.writerow([
+                funcionario.id,
+                funcionario.nome,
+                funcionario.data_nascimento,
+                funcionario.email,
+                funcionario.profissao,
+            ])
+        
+        return response
