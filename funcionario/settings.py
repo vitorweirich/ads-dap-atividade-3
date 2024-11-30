9d8a8273129c7fb34c3b0e1b1bfc1c63b6ba2a01
Vitor <werichvitor@gmail.com>
2024-11-29 23:00:11 -0300

    Configura projeto (banco de dados, rotas para o app bootstrap para estilizacao)

--- funcionario/settings.py
+++ funcionario/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'funcionario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'NAME': 'funcionarios',
     }
 }
 
